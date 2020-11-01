e17ca65c165760b88731cd58b5f4eb254000d821
anshusaurav <anshu.saurav@gmail.com>
2020-11-01 11:27:47 +0530

    Heroku Deployment

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(uri, {
    
   }
 })
-app.use(express.static("client/build"));
+app.use(express.static("build"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(express.static(path.join(__dirname, 'build')));
